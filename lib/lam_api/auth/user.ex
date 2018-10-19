372e09605f7bf2b97a931bb787e0575a8c31e798
Fernando Lopez <fer.ragnar19@gmail.com>
2018-10-18 21:03:03 -0300

    pruebas usuarios api

---
+++ lib/lam_api/auth/user.ex
@@ -0,0 +1,36 @@
+defmodule LamApi.Auth.User do
+  use Ecto.Schema
+  import Ecto.Changeset
+
+
+  schema "users" do
+    field(:email, :string)
+    field(:is_active, :boolean, default: false)
+    field(:password, :string, virtual: true)
+    field(:password_hash, :string)
+
+    timestamps()
+  end
+
+  @doc false
+  def changeset(user, attrs) do
+    user
+    |> cast(attrs, [:email, :is_active, :password])
+    |> validate_required([:email, :is_active, :password])
+    |> unique_constraint(:email)
+    |> put_password_hash()
+  end
+
+  defp put_password_hash(
+    %Ecto.Changeset{
+      valid?: true, changes: %{password: password}
+    } = changeset
+  ) do
+  change(changeset, password_hash: Bcrypt.hash_pwd_salt(password))
+    
+  end
+
+  defp put_password_hash(changeset) do
+    changeset
+  end
+end
