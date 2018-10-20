ec89b80926cbd754bcb143bc60e5a9f4b65ca6cb
Fernando Lopez <fer.ragnar19@gmail.com>
2018-10-20 13:13:35 -0300

    proteger recurso con autenticación

--- lib/lam_api_web/router.ex
+++ lib/lam_api_web/router.ex
@@ -6,6 +6,10 @@ defmodule LamApiWeb.Router do
     plug(:fetch_session)
   end
 
+  pipeline :api_auth do
+    plug(:ensure_authenticated)
+  end
+
   scope "/api", LamApiWeb do
     pipe_through :api
     resources "/users", UserController, except: [:new, :edit]
@@ -15,5 +19,18 @@ defmodule LamApiWeb.Router do
     pipe_through(:api)
     resources("/users", UserController, except: [:new, :edit])
     post("/users/sign_in", UserController, :sign_in)
+  end
+
+  defp ensure_authenticated(conn, _opts) do
+    current_user_id = get_session(conn, :current_user_id)
+
+    if current_user_id do
+      conn
+    else
+      conn
+      |> put_status(:unauthorized)
+      |> render(LamApiWeb.ErrorView, "401.json", message: "Usuario no logeado!")
+      |> halt()
+    end
   end
 end
